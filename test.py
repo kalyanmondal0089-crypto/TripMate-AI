d60a85dde9e16e05736b2b644b18e69081b6cd72
Kalyan Mondal <kalyanmondal0089@gmail.com>
2026-08-13 18:40:46 +0530

    app added

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
 from tools.tavily_tool import tavily_search
 from tools.flight_tools import search_flights
+from backend import run_travel_agent
 
 #res=tavily_search("Best hotels in India")
-res=search_flights("Plan a 7 days Japan trip from Bangladesh")
-print(res)
+#res=search_flights("Plan a 7 days Japan trip from Bangladesh")
+user_input = input("Enter travel request: ")
+
+response = run_travel_agent(
+    user_input=user_input,
+    thread_id="test_user"
+)
+
+print("\nFINAL RESPONSE:\n")
+print(response["answer"])
